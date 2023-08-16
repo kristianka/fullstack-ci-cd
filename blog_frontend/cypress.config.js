b36934cc5ee9f0d2447b36c123548b201e1c83a6
Kristian <49764796+kristianka@users.noreply.github.com>
2023-08-16 17:17:58 +0300

    fix cypress port

--- blog_frontend/cypress.config.js
+++ blog_frontend/cypress.config.js
@@ -3,7 +3,7 @@ import vitePreprocessor from "cypress-vite";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:5173/",
+    baseUrl: "http://localhost:3001/",
     setupNodeEvents(on) {
       on("file:preprocessor", vitePreprocessor());
     },
